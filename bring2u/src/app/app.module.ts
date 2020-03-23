ee05eca1783d5ad35dc64a4964051912c3f14080
Marcello Passos <marcellogpassos@gmail.com>
2020-03-22 21:46:20 -0300

    Mudança para IONIC

--- bring2u/src/app/app.module.ts
+++ bring2u/src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from '@angular/fire';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { environment } from 'src/environments/environment';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
-  imports: [
-    AngularFireModule.initializeApp(environment.firebase),
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- bring2u/src/environments/environment.ts
+++ bring2u/src/environments/environment.ts
@@ -3,17 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyA3RbrUAaV3T3RpiP2s50OGrzyFHYXB5m8',
-    authDomain: 'bring2u-dafaf.firebaseapp.com',
-    databaseURL: 'https://bring2u-dafaf.firebaseio.com',
-    projectId: 'bring2u-dafaf',
-    storageBucket: 'bring2u-dafaf.appspot.com',
-    messagingSenderId: '940587601196',
-    appId: '1:940587601196:web:f43efa128dd5d139d1b089',
-    measurementId: 'G-KLRW8T26T7'
-  }
+  production: false
 };
 
 /*
